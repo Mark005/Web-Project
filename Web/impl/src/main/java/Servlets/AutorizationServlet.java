e3a5786ee73a9963fc085431a8b76db8515c6910
Mark005 <likethewind322@gmail.com>
2020-03-13 20:49:24 +0300

    no message

--- Web/impl/src/main/java/Servlets/AutorizationServlet.java
+++ Web/impl/src/main/java/Servlets/AutorizationServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.Key;
@@ -18,9 +19,18 @@ public class AutorizationServlet extends HttpServlet {
         Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
         String jws = Jwts.builder().setSubject(req.getHeader("user")).signWith(key).compact();
 
+        StringBuffer jb = new StringBuffer();
+        String line = null;
+        try {
+            BufferedReader reader = req.getReader();
+            while ((line = reader.readLine()) != null)
+                jb.append(line);
+        } catch (Exception e) {
+
+        }
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println(req.getParameterMap());
+        out.println(jb.toString());
         out.println(jws);
 
     }
